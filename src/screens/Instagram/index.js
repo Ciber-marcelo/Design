9aefe29ab5771b420094baf3bb9f737d345b93ab
Marcelo <marcelones125@gmail.com>
2022-07-19 04:38:16 -0300

    Tela de login do Instagram aidicionada

--- src/screens/Instagram/index.js
+++ src/screens/Instagram/index.js
@@ -5,45 +5,57 @@ import logo from '../../assets/Instagram-Images/instagram.png';
 import foto from '../../assets/Instagram-Images/foto.png';
 import google from '../../assets/Instagram-Images/googlePlay.png';
 import store from '../../assets/Instagram-Images/appStore.png';
+import tela1 from '../../assets/Instagram-Images/perfil1.png';
+import tela2 from '../../assets/Instagram-Images/perfil2.png';
+import tela3 from '../../assets/Instagram-Images/perfil3.png';
 
 
 function Instagram() {
    return (
-      <div className='container'>
-         
-         <div className='wrapper'>
-            <div className='phone'>
-               <img src={phone} alt="celular" />
-            </div>
+      <div className='background'>
+         <div className='container'>
+            <div className='wrapper'>
+               <div className='phone'>
+                  <img src={tela1} className='tela1' alt="tela1" />
+                  <img src={tela3} className='tela2' alt="tela2" />
+                  <img src={tela2} className='tela3' alt="tela2" />
+                  <img src={phone} alt="celular" />
+               </div>
 
-            <div className='continue'>
-               <div className='group'>
-                  <img src={logo} className='logo' alt='logo' />
-                  <div className='foto'>
-                     <img src={foto} className='foto' alt='foto' />
+               <div className='continue'>
+                  <div className='group'>
+                     <img src={logo} className='logo' alt='logo' />
+                     <div className='foto'>
+                        <img src={foto} className='foto' alt='foto' />
+                     </div>
+                     <a href='a' className='login'>Continue como marcelones</a>
+                     <a href='a' className='logout'>Remover conta</a>
                   </div>
-                  <a href='a' className='login'>Continue como marcelones</a>
-                  <a href='a' className='logout'>Remover conta</a>
-               </div>
 
-               <div className='group'>
-                  <p className='not-account'>Não é marcelones ?</p>
-                  <p className='not-account'>
-                     <span className='link-blue'>Alterar contas </span>
-                     ou
-                     <span className='link-blue'> Inscreva-se</span>
-                  </p>
-               </div>
-
-               <div className='get-the-app'>
-                  <p>Baixe o aplicativo</p>
-                  <div className='downloads'>
-                     <img src={google} className='app-download' alt='google' />
-                     <img src={store} className='app-download' alt='store' />
+                  <div className='group'>
+                     <p className='not-account'>Não é marcelones ?</p>
+                     <p className='not-account'>
+                        <span className='link-blue'>Alterar contas </span>
+                        ou
+                        <span className='link-blue'> Inscreva-se</span>
+                     </p>
                   </div>
-               </div>
 
+                  <div className='get-the-app'>
+                     <p>Baixe o aplicativo</p>
+                     <div className='downloads'>
+                        <img src={google} className='app-download' alt='google' />
+                        <img src={store} className='app-download' alt='store' />
+                     </div>
+                  </div>
+
+               </div>
             </div>
+         </div>
+
+         <div className='footer'>
+            <p>&copy; 2022 Instagram</p>
+            <p>Desenvolvido por: Marcelones</p>
          </div>
       </div>
    );
